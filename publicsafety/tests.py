cca436672a4634f2982db597629aea3c5b733b6c
Madhu <->
2019-05-25 10:17:51 -0400

    initial basic tests to get ball moving

--- publicsafety/tests.py
+++ publicsafety/tests.py
@@ -2,6 +2,18 @@
 """
 Tests for publicsafety app
 """
-# from django.test import TestCase
 
-# Create your tests here.
+from django.test import SimpleTestCase
+
+
+class ViewTests(SimpleTestCase):
+    """
+    Testing views
+    """
+
+    def test_home_view(self):
+        """
+        Test api root url
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
